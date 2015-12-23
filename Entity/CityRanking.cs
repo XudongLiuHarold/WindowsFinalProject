cd90f67fc37a33f46fc1fb79bf4c0e53c87e8bc2
Harold LIU <7lxd@tongji.edu.cn>
2015-12-23 20:28:57 +0800

    Finish Network

--- Entity/CityRanking.cs
+++ Entity/CityRanking.cs
@@ -4,19 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PM25onWinPhone
+namespace PM25onWinPhone.Entity
 {
-    class SiteDetail
+    class CityRanking
     {
-      
         public string aqi;
         public string area;
         public string pm2_5;
         public string pm2_5_24h;
-        public string position_name;
         public string primary_pollutant;
         public string quality;
-        public string station_code;
         public string time_point;
         public string so2;
         public string no2;
@@ -30,6 +27,7 @@ namespace PM25onWinPhone
         public string o3_24h;
         public string o3_8h;
         public string o3_8h_24h;
+        public string level;
 
     }
 }
